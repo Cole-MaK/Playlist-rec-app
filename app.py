356498e910d9cd9a93b6897f487962026de31a13
Cole Mak <156642939+Cole-MaK@users.noreply.github.com>
2024-09-07 10:27:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app.secret_key = os.getenv('SECRET_KEY') # set secret key in .env file
 @app.route('/home')
 def home_page():
 
-    client_id = os.getenv('CLIENT_ID')
-    redirect_uri = 'http://127.0.0.1:5000/callback'
-    code_verifier = generate_code_verifier()
+    client_id = os.getenv('CLIENT_ID') # spotify client ID from creating spotify app through their API
+    redirect_uri = 'http://127.0.0.1:5000/callback' # callback after authorization
+    code_verifier = generate_code_verifier() # for pkce authorization
     code_challenge = generate_code_challenge(code_verifier)
 
     url = 'https://accounts.spotify.com/authorize' #get request url
@@ -26,9 +26,9 @@ def home_page():
         'code_challenge': code_challenge
     }
 
-    auth_url_with_params = f"{url}?{urllib.parse.urlencode(params)}"
+    auth_url_with_params = f"{url}?{urllib.parse.urlencode(params)}" # url to link for authorization
 
-    session['code-verifier'] = code_verifier
+    session['code-verifier'] = code_verifier # store code verifier for future use
 
     return render_template('index.html', url=auth_url_with_params)
 
@@ -36,15 +36,15 @@ def home_page():
 @app.route('/callback')
 def callback():
 
-    auth_code = request.args.get('code')
-    code_verifier = session.get('code-verifier', 'No data found')
+    auth_code = request.args.get('code') # get code from authorization
+    code_verifier = session.get('code-verifier', 'No data found') # retrieve code verifier to aquire access token from Spotify
     token = get_Token(auth_code, code_verifier)
-    session['token'] = token
+    session['token'] = token # store token 
 
     username = get_user_profile(token)
     session['username'] = username
     
-    playlist_items, other_items = get_user_playlists(token, username)
+    playlist_items, other_items = get_user_playlists(token, username) # get information of playlist and playlist to compare to
 
     return render_template('result.html', playlist_items = playlist_items, other_items = other_items)
 
@@ -57,10 +57,10 @@ def recommend():
     playlist_name = request.form['selected_playlist']
     playlist_other = request.form['selected_playlist-compare']
 
-    playlist_id = get_user_playlist_id(token, username, playlist_name, playlist_other)
-    daylist_df, playlist_df = create_playlist_df(token, playlist_id)
+    playlist_id = get_user_playlist_id(token, username, playlist_name, playlist_other) # get playlist id's
+    daylist_df, playlist_df = create_playlist_df(token, playlist_id) # create playlist data frames
 
-    df = generate_recommendation(token, playlist_id, daylist_df, playlist_df)
+    df = generate_recommendation(token, playlist_id, daylist_df, playlist_df) # preprocess, feature extract, one hot encode data to generate recommendations
 
     return render_template('recommend.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
